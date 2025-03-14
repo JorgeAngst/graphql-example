81bb777018ef3608402e09f72c6f446876fe84dc
JorgeAngst <jorge.rodriguez.web@gmail.com>
2025-03-14 12:08:09 +0100

    A graphql server app

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const { graphqlHTTP } = require('express-graphql');
+
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+
+const typesArray = loadFilesSync('**/*', {
+	extensions: ['graphql']
+});
+
+const resolversArray = loadFilesSync('**/*', {
+  extensions: ['resolvers.js']
+});
+
+const schema = makeExecutableSchema({
+	typeDefs: typesArray,
+  resolvers: resolversArray
+});
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log(`Running GraphQL server...`);
+});
